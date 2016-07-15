51963a7b6fcdea130dbd67d448df1bbd20f698c5
Annie Lu <luannie87@gmail.com>
2016-07-15 14:45:02 -0400

    kind of figured out hump gamestate
    :)

---
+++ testhump/main.lua
@@ -0,0 +1,30 @@
+Gamestate = require 'hump.gamestate'
+
+local menu = {} -- previously: Gamestate.new()
+local game = {}
+local ithinkigotthis = {}
+
+function menu:draw()
+    love.graphics.print("Press Enter to continue", 10, 10)
+end
+
+function menu:keyreleased(key, code)
+    if key == 'return' then
+        Gamestate.switch(game)
+    else
+        Gamestate.switch(ithinkigotthis)
+    end
+end
+
+function game:draw()
+    love.graphics.print("bless")
+end
+
+function ithinkigotthis:draw()
+  love.graphics.print("itworks :')")
+end
+
+function love.load()
+    Gamestate.registerEvents()
+    Gamestate.switch(menu)
+end
