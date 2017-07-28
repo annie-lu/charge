9946f2ce71593ce61c8d0409fea557329359b80e
Anna <ann8586@gmail.com>
2017-07-28 00:20:02 -0700

    making a new level just for the actual random charger moving thing, will implement the random component next

--- bedroomlevel/bedroom.lua
+++ bedroomlevel/bedroom.lua
@@ -4,15 +4,24 @@ local Bedroom = {}
 
 local Player = require 'stairslevel/playerTest2'
 local Charger = require 'bedroomlevel.charger'
+local Bedroom2 = require 'bedroomlevel.bedroom2'
 
 local p = Player(100, 350, true, false)
 local char = Charger(400, 500)
 
 local e = false
 
+local text = ""
+
 function Bedroom:update(dt)
   p:update(dt)
+  if love.keyboard.isDown('e') then
+    text = "nice u pressed e"
+  end
 
+  if p:getBB():collidesWith(char:getBB()) and love.keyboard.isDown('e') then
+    Gamestate.switch(Bedroom2)
+  end
 
 end
 
@@ -20,9 +29,7 @@ function Bedroom:draw()
   char:draw()
   p:draw()
   love.graphics.print("You found the charger! Press 'e' to pick it up!", 100, 80)
-  if love.keyboard.isDown('e') then
-    love.graphics.print("u pressed e nice", 100, 100)
-  end
+  love.graphics.print(text, 100, 100)
 end
 
 
