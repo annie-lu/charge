baf1f04030d99a054e50b6c30003b7bbf0e71daf
Anna <ann8586@gmail.com>
2017-08-04 16:59:05 -0700

    timing stuff

--- bedroomlevel/bedroom3.lua
+++ bedroomlevel/bedroom3.lua
@@ -1,10 +1,22 @@
 local Bedroom3 = {}
 
-local Timer = require 'timer'
+--local Timer = require 'timer'
 
 local testTimer = require 'hump.timer'
 
---local timeLimit = 10
+local timeLimit = 10
+
+local tT = testTimer.new()
+
+local text = ""..timeLimit
+
+function tick()
+  timeLimit = timeLimit - 1
+  text = ""..timeLimit
+end
+
+handle = tT:every(1, tick)
+
 --timer.every(1, function() timeLimit = timeLimit - 1 end, 10)
 
 --elaspedTime = 0
@@ -12,14 +24,24 @@ local testTimer = require 'hump.timer'
 function Bedroom3:update(dt)
 
   --elapsedTime = love.timer.getTime() - startTime
-  Timer:update(dt)
+
+
+  if timeLimit <= 0 then
+    tT:cancel(handle)
+  end
+
+  tT:update(dt)
 
 end
 
 function Bedroom3:draw()
-  Timer:draw()
+  --Timer:draw()
+
+
+
   love.graphics.setNewFont(14)
   love.graphics.setColor(255, 255, 255)
+  love.graphics.print(text, 100, 10)
   love.graphics.print("You got ur charger! But both your outlets are being used by your lamp and your computer!", 200, 100)
   --love.graphics.print("wtf "..love.timer.getTime(), 400, 135)
   love.graphics.print("You need to unplug one!", 400, 150)
