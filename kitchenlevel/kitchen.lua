34b30814dd5e469c4475bdbc85555801094d7598
Annie Lu <annie-lu@users.noreply.github.com>
2017-08-06 15:04:19 -0400

    getting rid of extra scenes
    ya boi

--- kitchenlevel/kitchen.lua
+++ kitchenlevel/kitchen.lua
@@ -17,7 +17,7 @@ local timer = Timer.new()
 local text = {}
 local name, name2, lol, name3
 local hit = 0
-local Kitchen3 = require 'kitchenlevel.kitchen3'
+local Kitchen3 = require 'pianolevel.piano1'
 --collider = HC.new(850)
 
 	local color = {0, 0, 0}
